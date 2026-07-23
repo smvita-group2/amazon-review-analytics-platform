dd9f0a01304b6de25a327e006c6be1c8eebf1b86
shreyash9999 <dongareshreyash20@gmail.com>
2026-07-23 04:41:20 +0000

    feat: dag folders and files created

---
+++ dags/config/default_args.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+
+default_args = {
+    "owner": "amazon-review-analytics",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+    "execution_timeout": timedelta(hours=2),
+}
