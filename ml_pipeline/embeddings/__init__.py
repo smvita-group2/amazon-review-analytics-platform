4a41b5f3ec5b600eff3cdbd914a3ee5086deb03e
shreyash9999 <dongareshreyash20@gmail.com>
2026-08-03 20:13:55 +0530

    fix(gitignore): unignore ml_pipeline/embeddings package so source files are tracked

---
+++ ml_pipeline/embeddings/__init__.py
@@ -0,0 +1,8 @@
+"""
+Embeddings package.
+"""
+
+from ml_pipeline.embeddings.embedding_generator import EmbeddingGenerator
+from ml_pipeline.embeddings.embedding_model import EmbeddingModel
+
+__all__ = ["EmbeddingGenerator", "EmbeddingModel"]
