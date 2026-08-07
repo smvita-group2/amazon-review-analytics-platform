0ad31eca1b669d9fee35bd62ea13413b58c52c9b
AffaanArbani <affaan3287@gmail.com>
2026-08-07 11:18:39 +0530

    update : formatting CI

--- ml_pipeline/check_models.py
+++ ml_pipeline/check_models.py
@@ -1,12 +1,11 @@
-from google import genai
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 
-client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 print("\nAvailable Models:\n")
 
